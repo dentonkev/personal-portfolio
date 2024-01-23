452868156e579eecbd3637dab86343c0877ea5e9
Denton Kev <dentonkev122@gmail.com>
2024-01-23 19:55:23 +1100

    Add elements to make the website responsive

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let menu = document.getElementById('menu');
+  let menuNav = document.querySelector('.menu_nav');
+  let exit = document.querySelector('.exit');
+
+  menu.addEventListener('click', () => {
+      menuNav.style.display = 'flex';
+  });
+
+  exit.addEventListener('click', () => {
+      menuNav.style.display = 'none';
+  });
+});
